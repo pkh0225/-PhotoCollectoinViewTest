728a80eb966c8c75b295009d5206ade6b34714de
pkh0225 <pkh0225@ssg.com>
2021-08-05 14:12:51 +0900

    .

--- PhotoCollectoinViewTest/Util/Extension/UIImageViewExtension.swift
+++ PhotoCollectoinViewTest/Util/Extension/UIImageViewExtension.swift
@@ -12,6 +12,13 @@ extension UIImageView {
     private struct AssociatedKeys {
         static var imageDataTask: UInt8 = 0
         static var url: UInt8 = 0
+        static var accessQueue: UInt = 0
+    }
+
+    enum CacheType {
+        case none
+        case memory
+        case disk
     }
 
     private static var UrlToImageCache: NSCache<NSString, UIImage>?
@@ -25,60 +32,217 @@ extension UIImageView {
         }
     }
 
-    private var url: URL? {
+    private var accessQueue: DispatchQueue? {
         get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.url) as? URL
+            if let q = objc_getAssociatedObject(self, &AssociatedKeys.imageDataTask) as? DispatchQueue {
+                return q
+            }
+            let queue =  DispatchQueue(label: "accessQueue_UIImageView", qos: .userInitiated, attributes: .concurrent)
+            objc_setAssociatedObject(self, &AssociatedKeys.url, queue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return queue
+
         }
         set {
             objc_setAssociatedObject(self, &AssociatedKeys.url, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
 
-
-    func getChachImage(urlString: String?) -> UIImage? {
+    func getMemoryCaach(urlString: String?) -> UIImage? {
         guard let urlString = urlString, urlString.isValid else { return  nil }
         return Self.UrlToImageCache?.object(forKey: urlString as NSString)
     }
 
-    func setUrlImage(_ urlString: String?, placeHolderImage: UIImage? = nil, backgroundColor: UIColor? = nil, transitionAnimation: Bool = true) {
-        imageDataTask?.cancel()
-        self.image =  placeHolderImage
-        self.backgroundColor = backgroundColor
+    func saveMemoryCache(urlString: String?, image: UIImage?) {
+        guard let urlString = urlString, let image = image else { return }
+        if Self.UrlToImageCache == nil {
+            Self.UrlToImageCache = NSCache<NSString, UIImage>()
+            Self.UrlToImageCache?.countLimit = 100
+            Self.UrlToImageCache?.totalCostLimit = 50 * 1024 * 1024;
+        }
+        if Self.UrlToImageCache?.object(forKey: urlString as NSString) == nil {
+            Self.UrlToImageCache?.setObject(image, forKey: urlString as NSString)
+        }
+    }
 
-        guard let urlString = urlString, urlString.isValid else { return }
-        if let cachedImage = getChachImage(urlString: urlString) {
-            self.image = cachedImage
+    func momoryCacheClear() {
+        Self.UrlToImageCache?.removeAllObjects()
+    }
+
+    func getFileName(urlString: String?) -> String? {
+        guard let urlString = urlString, let url = URL(string: urlString) else { return nil }
+        var fileName: String = ""
+        for (idx,path) in url.pathComponents.enumerated() {
+            guard idx > 0 else { continue }
+            fileName += "_\(path)"
+        }
+        return fileName
+    }
+
+    func saveDiskCaghe(urlString: String?, image: UIImage) {
+        guard let urlString = urlString else { return }
+
+        guard let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return }
+
+        guard let fileName = self.getFileName(urlString: urlString) else { return }
+        var filePath = URL(fileURLWithPath: path)
+        filePath.appendPathComponent("image")
+
+
+        if !FileManager.default.fileExists(atPath: filePath.path) {
+            try? FileManager.default.createDirectory(atPath: filePath.path, withIntermediateDirectories: false, attributes: nil)
+        }
+
+        filePath.appendPathComponent(fileName)
+        guard !FileManager.default.fileExists(atPath: filePath.path) else { return }
+        FileManager.default.createFile(atPath: filePath.path, contents: image.jpegData(compressionQuality: 1), attributes: nil)
+    }
+
+    func getDiskCache(urlString: String?) -> Result<UIImage?, NSError> {
+        guard let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else {
+            return (.failure(NSError(domain: "path Directories error", code: -2000, userInfo: nil)))
+        }
+
+        guard let fileName = self.getFileName(urlString: urlString) else {
+            return (.failure(NSError(domain: "get file name error", code: -3000, userInfo: nil)))
+        }
+
+        var filePath = URL(fileURLWithPath: path)
+        filePath.appendPathComponent("image")
+        filePath.appendPathComponent(fileName)
+        guard FileManager.default.fileExists(atPath: filePath.path) else {
+            return (.failure(NSError(domain: "file path error", code: -4000, userInfo: nil)))
+        }
+
+        guard let imageData = try? Data(contentsOf: filePath) else {
+            return (.failure(NSError(domain: "imageData error", code: -5000, userInfo: nil)))
+        }
+
+        guard let image = UIImage(data: imageData) else {
+            return (.failure(NSError(domain: "data to convert error", code: -6000, userInfo: nil)))
+        }
+
+        return (.success(image))
+    }
+
+    static func diskCacheClear() {
+        guard let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return }
+        var filePath = URL(fileURLWithPath: path)
+        filePath.appendPathComponent("image")
+        try? FileManager.default.removeItem(at: filePath)
+    }
+
+    func getCacheImage(urlString: String, completion: @escaping (CacheType) -> Void) {
+        self.accessQueue?.async(flags: .barrier) {
+            if let image = self.getMemoryCaach(urlString: urlString) {
+                print("image memory Cache: \(urlString)")
+                DispatchQueue.main.sync {
+                    self.image = image
+                    self.backgroundColor = .none
+                }
+                return completion(.memory)
+            }
+
+            let result = self.getDiskCache(urlString: urlString)
+            switch result {
+            case .success(let image):
+                print("image disk Cache: \(urlString)")
+                DispatchQueue.main.sync {
+                    self.image = image
+                    self.backgroundColor = .none
+                }
+                self.saveMemoryCache(urlString: urlString, image: image)
+                return completion(.disk)
+
+            case .failure(_):
+//                            print("getDiskCacheImage error : \(error)")
+                return completion(.none)
+            }
+        }
+    }
+
+    func saveCacheImage(urlString: String?, image: UIImage) {
+        self.accessQueue?.async(flags: .barrier) {
+            self.saveMemoryCache(urlString: urlString, image: image)
+            self.saveDiskCaghe(urlString: urlString, image: image)
+        }
+    }
+
+    func setUrlImage(_ urlString: String?, placeHolderImage: UIImage? = nil, backgroundColor: UIColor? = nil, transitionAnimation: Bool = true, cache: Bool = true, isEtage: Bool = false) {
+        imageDataTask?.cancel()
+        guard let urlString = urlString, urlString.isValid, let url = URL(string: urlString) else {
+            self.image =  placeHolderImage
+            self.backgroundColor = backgroundColor
             return
         }
 
-        guard let url = URL(string: urlString) else { return }
-        self.url = url
-        imageDataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
-            guard let self = self else { return }
-            guard let data = data, let image = UIImage(data: data), error == nil else { return }
-            self.imageDataTask = nil
-
-            if Self.UrlToImageCache == nil {
-                Self.UrlToImageCache = NSCache<NSString, UIImage>()
-                Self.UrlToImageCache?.countLimit = 100
-            }
-            Self.UrlToImageCache?.setObject(image, forKey: urlString as NSString)
-
-            if self.url == url {
-                DispatchQueue.main.async {
-                    if transitionAnimation {
-                        UIView.transition(with: self, duration: 0.25, options: [.transitionCrossDissolve], animations: {
-                            self.image = image
-                        }, completion: nil)
-                    }
-                    else {
-                        self.image = image
-                    }
-
+        if !isEtage {
+            getCacheImage(urlString: urlString) { cacheType in
+                if cacheType == .none {
+                    self.setUrlImage(urlString, placeHolderImage: placeHolderImage, backgroundColor: backgroundColor, transitionAnimation: transitionAnimation, cache: false, isEtage: true)
                 }
             }
         }
+        else {
+            DispatchQueue.main.sync {
+                self.image =  placeHolderImage
+                self.backgroundColor = backgroundColor
+            }
 
-        imageDataTask?.resume()
+            var urlRequest = URLRequest(url: url)
+            if isEtage, cache, let eTag = UserDefaults.standard.object(forKey: "Etag_\(urlString)") as? String {
+                urlRequest.addValue(eTag, forHTTPHeaderField: "If-None-Match")
+            }
+
+            self.imageDataTask = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, response, error) in
+                guard let self = self else { return }
+                self.imageDataTask = nil
+
+                if let _ = error {
+                    //                print("iamge download error: " + error.localizedDescription + "\n")
+                    return
+                }
+                else if let response = response as? HTTPURLResponse {
+                    // 변경되지 않음
+                    if cache, response.statusCode == 304 {
+                        //                    print("변경되지 않음")
+                        self.getCacheImage(urlString: urlString) { cacheType in
+                            if cacheType == .none {
+                                DispatchQueue.main.async {
+                                    self.setUrlImage(urlString, placeHolderImage: placeHolderImage, backgroundColor: backgroundColor, transitionAnimation: transitionAnimation, cache: false)
+                                }
+                            }
+                        }
+                    }
+                    else if response.statusCode == 200, let data = data, let image = UIImage(data: data) {
+                        DispatchQueue.main.sync {
+                            if transitionAnimation {
+                                UIView.transition(with: self, duration: 0.25, options: [.transitionCrossDissolve], animations: {
+                                    self.image = image
+                                    self.backgroundColor = .none
+                                }, completion: nil)
+                            }
+                            else {
+                                self.image = image
+                                self.backgroundColor = .none
+                            }
+                        }
+
+                        if isEtage, let etag = response.allHeaderFields["Etag"] as? String {
+                            UserDefaults.standard.setValue(etag, forKey: "Etag_\(urlString)")
+                        }
+
+                        self.saveCacheImage(urlString: urlString, image: image)
+                    }
+                    else {
+                        print("response.statusCode: \(response.statusCode)")
+                    }
+                }
+
+
+            }
+
+            self.imageDataTask?.resume()
+        }
     }
+
 }
