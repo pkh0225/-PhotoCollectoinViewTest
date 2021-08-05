51b8821c28b69303bcae20ddb209c96177746e1e
pkh0225 <pkh0225@ssg.com>
2021-08-05 16:12:14 +0900

    .

--- PhotoCollectoinViewTest/AppDelegate.swift
+++ PhotoCollectoinViewTest/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIImageView.checkDiskCacheAndRemove(totalCostLimit: 200)
+        NotificationCenter.default.addObserver(forName:UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { (_) in
+            UIImageView.checkDiskCacheAndRemove(totalCostLimit: 200)
+        }
+        NotificationCenter.default.addObserver(forName:UIApplication.willTerminateNotification, object: nil, queue: nil) { (_) in
+            UIImageView.checkDiskCacheAndRemove(totalCostLimit: 200)
+        }
+
         return true
     }
 
@@ -31,8 +38,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
-
 }
 
